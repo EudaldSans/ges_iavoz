9d5c3c36236e3f28a4eecf597da823cc4b8e5061
Eudald Sans <eudald.sans@gmail.com>
2022-10-26 10:25:16 +0200

    Keep track of positives during execution

--- main/main.c
+++ main/main.c
@@ -4,14 +4,14 @@
 #include "ges_iavoz.h"
 
 static const char * TAG = "Main";
+uint32_t positives = 0;
 
-void iavoz_callback_dummy ( IAVOZ_KEY_t xKeyWord, uint64_t uiPower)
-{
-    ESP_LOGI(TAG, "Received %d with %llu", (int) xKeyWord, uiPower);
+void iavoz_callback_dummy ( IAVOZ_KEY_t xKeyWord, uint64_t uiPower) {
+    positives++;
+    ESP_LOGI(TAG, "Received %d with %llu, total positives: ", (int) xKeyWord, uiPower, positives);
 }
 
-void app_main ( void )
-{
+void app_main ( void ) {
     ESP_LOGI(TAG, "Starting System");
     IAVOZ_Init(1, iavoz_callback_dummy);
 }
